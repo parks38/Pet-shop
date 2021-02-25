e810f1e70970a97a3ddd306308b655480b70a2f8
yhh1056 <yhh1056@naver.com>
2021-02-25 22:31:24 +0900

    Refac: 패키지 구조 변경

--- src/main/java/com/petshop/controller/BoardController.java
+++ src/main/java/com/petshop/controller/BoardController.java
@@ -1,7 +1,8 @@
-package com.petshop;
+package com.petshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -12,5 +13,13 @@ public class BoardController {
          * todo 전체 게시물을 가져온다.
          */
         return "/boards";
+    }
+
+    @GetMapping("/boards/{id}")
+    public String getBoardsDetail(@PathVariable Long id) {
+        /**
+         * todo 해당 id의 게시물을 가져온다.
+         */
+        return "/item";
     }
 }
--- src/main/java/com/petshop/controller/HomeController.java
+++ src/main/java/com/petshop/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.petshop;
+package com.petshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
